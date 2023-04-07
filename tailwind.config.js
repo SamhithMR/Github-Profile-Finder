1da07c77bb03a1ec0c1111e16928eef5b8d372c5
samhithMR <samhith.mr@gmail.com>
2023-04-07 22:01:11 +0530

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+tailwind.config = {
+    theme: {
+      screens: {
+        sm: '338px',
+        md: '768px',
+        lg: '1024px',
+        xl: '1440px'
+      },
+      extend: {
+        colors: {
+          background: "#06090F",
+          dark: "#161B22",
+          green: "#2EA043"
+        },
+        fontFamily: {
+          'sans': ['"Roboto"', 'sans-serif'],
+          'rubik': ['Rubik', 'sans-serif'],
+          'Montserrat': ['Montserrat', 'sans-serif'],
+        }
+      }
+    },
+    variants: {},
+    plugins: []
+  }
